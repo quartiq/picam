4bacd353234c0fd4302ff76426ecf93d5fcca8d6
Robert Jordens <jordens@gmail.com>
2018-04-12 18:17:20 +0000

    pi: support parameter handling

--- pi.py
+++ pi.py
@@ -1,6 +1,6 @@
 import logging
 
-from ctypes import byref, POINTER, cast, c_char_p, string_at
+from ctypes import byref, POINTER, cast, c_char_p, string_at, c_double
 import numpy as np
 
 import picam as pi
@@ -14,8 +14,11 @@ class Error(Exception):
         self.error = error
 
     def __str__(self):
-        return "Error({}): {}".format(
-           self.error, get_string(pi.PicamEnumeratedType_Error, self.error))
+        try:
+            string = get_string(pi.PicamEnumeratedType_Error, self.error)
+        except:
+            string = "<unknown error>"
+        return "Error({}): {}".format(self.error, string)
 
     @classmethod
     def check(cls, err):
@@ -87,11 +90,51 @@ class Camera:
         Error.check(pi.Picam_GetCameraID(self._handle, byref(cid)))
         return cid
 
-    def get_integer(self, typ):
+    def get_int(self, typ):
         val = pi.piint()
         Error.check(pi.Picam_GetParameterIntegerValue(
             self._handle, typ, byref(val)))
         return val.value
+
+    def get_long(self, typ):
+        val = pi.pi64s()
+        Error.check(pi.Picam_GetParameterIntegerValue(
+            self._handle, typ, byref(val)))
+        return val.value
+
+    def get_float(self, typ):
+        val = pi.piflt()
+        Error.check(pi.Picam_GetParameterFloatingPointValue(
+            self._handle, typ, byref(val)))
+        return val.value
+
+    def set_int(self, typ, value):
+        Error.check(pi.Picam_SetParameterIntegerValue(
+            self._handle, typ, pi.piint(value)))
+
+    def set_long(self, typ, value):
+        Error.check(pi.Picam_SetParameterLargeIntegerValue(
+            self._handle, typ, pi.pi64s(value)))
+
+    def set_float(self, typ, value):
+        Error.check(pi.Picam_SetParameterFloatingPointValue(
+            self._handle, typ, pi.piflt(value)))
+
+    def comitted(self):
+        ret = pi.pibln()
+        Error.check(pi.Picam_AreParametersCommitted(
+            self._handle, byref(ret)))
+        return ret.value
+
+    def commit(self):
+        failed = POINTER(pi.PicamParameter)()
+        failed_count = pi.piint()
+        Error.check(pi.Picam_CommitParameters(
+            self._handle, byref(failed), byref(failed_count)))
+        failed_names = [get_string(pi.PicamEnumeratedType_Parameter,
+            failed[i]) for i in range(failed_count.value)]
+        Error.check(pi.Picam_DestroyParameters(failed))
+        return failed_names
 
     def acquire(self, num_frames=1, timeout=-1):
         data = pi.PicamAvailableData()
@@ -99,20 +142,34 @@ class Camera:
         logger.debug("acquire")
         Error.check(pi.Picam_Acquire(self._handle, num_frames, timeout,
                                    byref(data), byref(errors)))
+        if errors.value == pi.PicamAcquisitionErrorsMask_None:
+            errors = None
+        else:
+            errors = get_string(pi.PicamEnumeratedType_AcquisitionErrorsMask,
+                                errors)
         return data, errors
 
 
 def main():
-    NUM_FRAMES = 5
+    NUM_FRAMES = 3
 
     with Library() as lib, Camera().open_first() as cam:
         cid = cam.get_id()
         model = get_string(pi.PicamEnumeratedType_Model, cid.model)
         logger.info("model: %s, serial: %s, sensor: %s",
                     model, cid.serial_number, cid.sensor_name)
-        readoutstride = cam.get_integer(pi.PicamParameter_ReadoutStride)
+        readoutstride = cam.get_int(pi.PicamParameter_ReadoutStride)
+        logger.debug("exposure %s ms",
+                      cam.get_float(pi.PicamParameter_ExposureTime))
+        cam.set_int(pi.PicamParameter_AdcAnalogGain,
+                    pi.PicamAdcAnalogGain_Low)
+        cam.set_float(pi.PicamParameter_ExposureTime, 30.)  # ms
+        cam.set_long(pi.PicamParameter_ReadoutCount, 1)
+        cam.commit()
         for i in range(NUM_FRAMES):
             data, errors = cam.acquire(num_frames=1)
+            if errors is not None:
+                logger.warning("acquisition errors %s", errors)
             data = get_data(
                 data.initial_readout, data.readout_count, readoutstride)
             logger.info("frames %s: %s", data.shape, data[:, :10])
