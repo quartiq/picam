4665553f997c0dea6267904610d0e0907f5f653a
Robert Jordens <jordens@gmail.com>
2018-04-15 17:36:51 +0000

    setup.py

--- README.md
+++
@@ -1,6 +0,0 @@
-# picam/types.py
-
-Generated using ctypeslib2 https://github.com/trolldbois/ctypeslib
-with:
-
-`clang2py.py -l libpicam.so.0 Picam_SDK/pi/includes/pi* > picam/types.py`
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="picam",
+    version="0.1",
+    description="PICam Python wrappers for Princeton Instruments PICam library",
+    long_description=open("README.rst").read(),
+    author="QUARTIQ GmbH",
+    author_email="rj@quartiq.de",
+    url="https://github.com/quartiq/picam",
+    download_url="https://github.com/quartiq/picam",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            # "aqctl_picam = ptb.aqctl_picam:main",
+        ],
+    },
+    # test_suite="picam.test",
+    license="LGPLv3+",
+)
